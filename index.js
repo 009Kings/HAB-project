a08ffede886236e852a0cc3246fe00e87706c130
009kings <009kings@gmail.com>
2018-12-19 11:44:06 -0800

    EJS is gross

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-var express = require('express');
+var express = require(`express`);
 var app = express();
 
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+app.set(`view engine`, `ejs`)
+
+app.get(`/`, function (req, res) {
+    res.render(`home`, {name: `Sarah`, age: 27, favouriteAnimal: `Wolf`});
 })
 
-app.get("/about", function (req, res) {
-    res.sendFile(__dirname + '/views/about.html');
+app.get(`/about`, function (req, res) {
+    res.render(`about`, {favouriteThings: [`Buddhism`, `Games`, `Art`]});
 })
 
-app.get("/blog", function (req, res) {
-    res.sendFile(__dirname + '/views/blog-directory.html');
+app.get(`/blog`, function (req, res) {
+    res.render(`blog-directory`);
 })
 
-app.listen("8000");
+app.listen(`8000`, function(){
+    console.log(`You're listening to the smooth sounds of port 8000`);
+});
